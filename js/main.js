a939282f9a56ffef3f6c0d1b08aef021daf8547a
mdeveloper66 <m.developer66@gmail.com>
2022-05-26 02:49:55 +0430

    Media queries completed

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 const navbar = document.querySelector('.navbar');
+const navMenu = document.querySelector('.menu');
+const navToggle = document.querySelector('.menu-btn');
+const navLink = document.querySelectorAll('.nav-link');
+
 const pic = document.querySelector('#main-shoe');
 const cyan = document.querySelector('.cyan');
 const purple = document.querySelector('.purple');
@@ -13,10 +17,21 @@ window.onscroll = () => {
     this.scrollY > 20 ? navbar.classList.add('sticky') : navbar.classList.remove('sticky');
 }
 
-window.onload = ()=> {
+window.onload = () => {
     this.scrollY > 20 ? navbar.classList.add('sticky') : navbar.classList.remove('sticky');
 }
 
+// navbar toggler
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.toggle('active');
+    })
+}
+
+// close menu on click item
+navLink.forEach(item=> item.addEventListener('click', ()=> {
+    navMenu.classList.remove('active');
+}))
 
 // store product info in object
 const info = {
@@ -29,7 +44,7 @@ const info = {
 };
 
 
-// change color
+// change product color
 cyan.addEventListener('click', () => {
     pic.src = info['cyan']
 })
